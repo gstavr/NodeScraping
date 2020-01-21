99146fdcfffe01b97d06a0df1ca0fc3d4d85d806
Gewrgios <gstavr1988@gmail.com>
2020-01-21 13:18:14 +0200

    Add files via upload

---
+++ example.js
@@ -0,0 +1,38 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null
+  });
+  const page = await browser.newPage();
+  await page.goto("https://www.ab.gr/");
+  //await page.pdf({ path: "hn.pdf", format: "A4" });
+
+  //document.querySelectorAll('ul[data-level="1"] > li > a');
+  await page.waitFor('ul[data-level="1"] > li > a');
+
+  const click2Shophrefs = await page.$$eval(
+    'ul[data-level="1"] > li > a',
+    href => {
+      return href.map(links => {
+        let obj = {};
+        obj.href = links.href;
+        obj.text = links.text;
+        return obj;
+      });
+    }
+  );
+
+  console.log(click2Shophrefs);
+
+  click2Shophrefs.forEach(async link => {
+    console.log(`Going to ${link.href}`);
+    await page.goto(link.href);
+    await page.waitFor(link.href);
+    console.log(`Gone to ${link.href}`);
+    //setTimeout(() => {}, 5000);
+  });
+
+  //await browser.close();
+})();
